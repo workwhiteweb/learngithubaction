a1c863131003a3d7c5cf0974884072de1f7ca407
workwhiteweb <workwhiteweb@gmail.com>
2022-08-03 15:18:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ def bye():
     print("bye")
     
 def total():
-    a = [1,2,3,4,5]
-    print(sum(a))
+    numbers = [1,2,3,4,5,1,4,5]
+    Sum = sum(numbers)
+    average= Sum/len(numbers)
+    print (average)
 
 
 print(hello())
